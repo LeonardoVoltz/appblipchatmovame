1a9f2e65608f598386811970671d50cd4b7bb10d
LeonardoVoltz <leonardo.becks@gmail.com>
2022-12-21 16:33:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,63 +17,7 @@ export default function Home() {
         </p>
       </main>
 
-      <Footer>
-    <script src = "https://unpkg.com/blip-chat-widget"
-
-type = "text/javascript" >
-
-</script>  
-
-<script>
-
-    (function() {
-
-        window.onload = function() {
-
-            var queryString = window.location.search;
-
-            var urlParams = new URLSearchParams(queryString);
-
-            new BlipChat()
-
-                .withAppKey('bW92YTI6ZjAxMjY5YjAtZGU4NC00ZTkyLWFjM2ItOGMwZjdlNmU3YzRj')
-
-                .withButton({
-
-                    "color": "#2CC3D5",
-
-                    "icon": ""
-
-                })
-
-                .withCustomCommonUrl('https://mova.chat.blip.ai/')
-
-                .withAccount({
-
-                    "extras": {
-
-                        "utm_source": urlParams.get('utm_source'),
-
-                        "utm_medium": urlParams.get('utm_medium'),
-
-                        "utm_campaign": urlParams.get('utm_campaign'),
-
-                        "utm_term": urlParams.get('utm_term'),
-
-                        "utm_content": urlParams.get('utm_content')
-
-                    }
-
-                })
-
-                .build();
-
-            }
-
-    })();
-
-</script>
-    </Footer>
+      <Footer/>
     </div>
   )
 }
